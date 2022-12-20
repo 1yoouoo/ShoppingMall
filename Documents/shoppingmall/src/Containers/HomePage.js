4218dc462a47078f83c338b194ca7ab3560a9651
이지윤 <1yoouoo@gmail.com>
2022-12-20 20:03:24 +0900

    design : wallpaper blur

--- Documents/shoppingmall/src/Containers/HomePage.js
+++ Documents/shoppingmall/src/Containers/HomePage.js
@@ -3,15 +3,26 @@ const HomePage = () => {
     <div className="homepage">
       <form className="homepage__form">
         <div className="homepage__form--img">
+          <div className="homepage__form--wrapper">
+            <span className="homepage__form--text">ALL ITEM</span>
+            <button className="homepage__form--button">MORE</button>
+          </div>
           <img
-            src={require("/Users/blanc/Documents/shoppingmall/src/Assets/zomboid_wallpaper_1.jpeg")}
-            alt=""
+            id="wallpaper"
+            src={require("../Assets/zomboid_wallpaper_1.jpeg")}
+            alt="zomboid1"
           />
         </div>
+
         <div className="homepage__form--img">
+          <div className="homepage__form--wrapper">
+            <span className="homepage__form--text">BEST ITEM</span>
+            <button className="homepage__form--button">MORE</button>
+          </div>
           <img
-            src={require("/Users/blanc/Documents/shoppingmall/src/Assets/zomboid_wallpaper_2.jpeg")}
-            alt=""
+            id="wallpaper"
+            src={require("../Assets/zomboid_wallpaper_2.jpeg")}
+            alt="zomboid2"
           />
         </div>
       </form>
