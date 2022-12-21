685f58bc4cc1492d9e24c4f0bccfdec1b7e814cf
이지윤 <1yoouoo@gmail.com>
2022-12-21 20:05:19 +0900

    design : moblie 반응형 헤더 진행중

--- Documents/shoppingmall/src/Common/Header.js
+++ Documents/shoppingmall/src/Common/Header.js
@@ -23,6 +23,11 @@ const Header = () => {
           <b>ZOMBO</b>
         </div>
         <ul className="header-form__left">
+          <span className="header-form__icon--menubar">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+              <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+            </svg>
+          </span>
           <NavigatorItem title="SHOP" items={shopList} />
           <NavigatorItem title="COMMUNITY" items={communityList} />
           <NavigatorItem title="MY PAGE" items={myPageList} />
