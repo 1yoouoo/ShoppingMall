fddb1b5368ed57a2ac83e54bcb194ebaf8e18ba4
이지윤 <1yoouoo@gmail.com>
2022-12-22 14:24:46 +0900

    feat: header menubar toggle

--- Documents/shoppingmall/src/Common/Header.js
+++ Documents/shoppingmall/src/Common/Header.js
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavigatorItem from "../Components/NavigatorItem";
 
 const Header = () => {
+  //state
+  const [toggle, setToggle] = useState(false);
   const navigate = useNavigate();
   const shopList = [
     "ALL PADDING 5%",
@@ -16,6 +19,11 @@ const Header = () => {
   ];
   const communityList = ["NOTICE", "Q&A", "REVIEW"];
   const myPageList = ["주문조회", "관심상품", "적립금", "배송조회"];
+
+  //function
+  const onClickToggle = () => {
+    setToggle(!toggle);
+  };
   return (
     <header className="header">
       <div className="header-form">
@@ -23,34 +31,39 @@ const Header = () => {
           <b>ZOMBO</b>
         </div>
         <ul className="header-form__left">
-          <span className="header-form__icon--menubar">
+          <span className="header-form__icon--menubar" onClick={onClickToggle}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
             </svg>
           </span>
-          <ul className="header-form__menubar--list">
-            <li className="navigator-item__mobile--wrapper">
-              <NavigatorItem
-                title="SHOP"
-                items={shopList}
-                className="navigator-item__mobile"
-              />
-            </li>
-            <li className="navigator-item__mobile--wrapper">
-              <NavigatorItem
-                title="COMMUNITY"
-                items={communityList}
-                className="navigator-item__mobile"
-              />
-            </li>
-            <li className="navigator-item__mobile--wrapper">
-              <NavigatorItem
-                title="MY PAGE"
-                items={myPageList}
-                className="navigator-item__mobile"
-              />
-            </li>
-          </ul>
+          {toggle ? (
+            <ul className="header-form__menubar--list">
+              <li className="navigator-item__mobile--wrapper">
+                <NavigatorItem
+                  title="SHOP"
+                  items={shopList}
+                  className="navigator-item__mobile"
+                />
+              </li>
+              <li className="navigator-item__mobile--wrapper">
+                <NavigatorItem
+                  title="COMMUNITY"
+                  items={communityList}
+                  className="navigator-item__mobile"
+                />
+              </li>
+              <li className="navigator-item__mobile--wrapper">
+                <NavigatorItem
+                  title="MY PAGE"
+                  items={myPageList}
+                  className="navigator-item__mobile"
+                />
+              </li>
+            </ul>
+          ) : (
+            <></>
+          )}
+
           <NavigatorItem
             title="SHOP"
             items={shopList}
