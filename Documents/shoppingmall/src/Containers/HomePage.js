915f4cab5570ea9cf8700b2b33103b1808fa7c8f
이지윤 <1yoouoo@gmail.com>
2022-12-22 17:10:16 +0900

    feat: mobile) homepage slide

--- Documents/shoppingmall/src/Containers/HomePage.js
+++ Documents/shoppingmall/src/Containers/HomePage.js
@@ -1,12 +1,45 @@
+import { useState } from "react";
+
 const HomePage = () => {
+  //state
+  const [slide, setSlide] = useState(0);
+  //function
+  const onClickRight = () => {};
   return (
     <div className="homepage">
       <form className="homepage__form">
-        <div className="homepage__form--img">
+        <div
+          className="homepage__form--img"
+          style={{ transform: `translate(${slide * -102}%)` }}
+        >
           <div className="homepage__form--wrapper">
-            <span className="homepage__form--text">ALL ITEM</span>
-            <button className="homepage__form--button">MORE</button>
+            <div className="homepage__left--icon-invisible">
+              <span className="homepage__form--left">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+                </svg>
+              </span>
+            </div>
+            <div className="homepage__form--left">
+              <span className="homepage__form--text">ALL ITEM</span>
+              <button className="homepage__form--button">MORE</button>
+            </div>
+
+            <div
+              className="homepage__right--icon"
+              onClick={() => {
+                console.log(slide);
+                setSlide(slide + 1);
+              }}
+            >
+              <span className="homepage__form--right">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+                </svg>
+              </span>
+            </div>
           </div>
+
           <img
             id="wallpaper"
             src={require("../Assets/zomboid_wallpaper_1.jpeg")}
@@ -14,11 +47,39 @@ const HomePage = () => {
           />
         </div>
 
-        <div className="homepage__form--img">
+        <div
+          className="homepage__form--img"
+          style={{ transform: `translate(${slide * -102}%)` }}
+        >
           <div className="homepage__form--wrapper">
-            <span className="homepage__form--text">BEST ITEM</span>
-            <button className="homepage__form--button">MORE</button>
+            <div
+              className="homepage__left--icon"
+              onClick={() => {
+                console.log(slide);
+                setSlide(slide - 1);
+              }}
+            >
+              <span className="homepage__form--left">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+                </svg>
+              </span>
+            </div>
+
+            <div className="homepage__form--right">
+              <span className="homepage__form--text">BEST ITEM</span>
+              <button className="homepage__form--button">MORE</button>
+            </div>
+
+            <div className="homepage__right--icon-invisible">
+              <span className="homepage__form--right">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+                </svg>
+              </span>
+            </div>
           </div>
+
           <img
             id="wallpaper"
             src={require("../Assets/zomboid_wallpaper_2.jpeg")}
