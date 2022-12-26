dcd106bdbf4698d2d21ba4966f4ca0b04d12aaa4
이지윤 <1yoouoo@gmail.com>
2022-12-26 18:05:30 +0900

    feat: 이미지 업로드 진행중

--- Documents/shoppingmall/src/Containers/ProductRegistrationPage.js
+++ Documents/shoppingmall/src/Containers/ProductRegistrationPage.js
@@ -1,6 +1,20 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 const ProductRegistrationPage = () => {
+  const [fileImage, setFileImage] = useState("");
+
+  // 데이터 절약 useMemo
+  const previewImage = useMemo(() => {
+    return fileImage ? (
+      <span className="product-registration__form--sample">
+        <img alt="sample" src={fileImage} />
+      </span>
+    ) : (
+      <span className="product-registration__form--sample">
+        <img src={require("../Assets/upload-img.png")} alt="" />
+      </span>
+    );
+  }, [fileImage]);
   const [inputValue, setInputValue] = useState({
     title: "",
     detail: "",
@@ -16,12 +30,17 @@ const ProductRegistrationPage = () => {
   const onSubmit = () => {
     console.log(inputValue);
   };
+  const saveFileImage = (e) => {
+    setFileImage(URL.createObjectURL(e.target.files[0]));
+  };
   return (
     <div className="product-registration">
       <div className="product-registration__form">
         <div className="product-registration__form--left">
+          <h2>이미지 업로드</h2>
+          {previewImage}
           <span>
-            <input type="file" />
+            <input type="file" accept="/image/*" onChange={saveFileImage} />
           </span>
         </div>
         <div className="product-registration__form--right">
