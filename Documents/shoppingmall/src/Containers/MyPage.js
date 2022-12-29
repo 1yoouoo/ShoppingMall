6d160a6566206cdcde0b98b9591de9b57cbe6a4f
이지윤 <1yoouoo@gmail.com>
2022-12-29 16:12:26 +0900

    feat: 유저 상세정보 페이지

--- Documents/shoppingmall/src/Containers/MyPage.js
+++ Documents/shoppingmall/src/Containers/MyPage.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import API from "../API/Api";
 
 const MyPage = () => {
@@ -6,25 +6,94 @@ const MyPage = () => {
     const token = localStorage.getItem("token");
     API.getuser(token).then((data) => {
       console.log(data);
+      setUserData({
+        ...userData,
+        login_id: data.data.data.login_id,
+        name: data.data.data.name,
+        phone_number: data.data.data.phone_number,
+        email: data.data.data.email,
+      });
     });
+  }, []);
+  const [changeUserData, setChangeUserData] = useState(false);
+  const [userData, setUserData] = useState({
+    login_id: "",
+    name: "",
+    phone_number: "",
+    email: "",
+    password: "",
   });
+  //function
+  const onClickChange = () => {
+    setChangeUserData(!changeUserData);
+  };
+  const onClickSave = () => {
+    setChangeUserData(!changeUserData);
+    console.log(userData);
+  };
+  const onChange = (e) => {
+    setUserData({
+      ...userData,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
-    <div>
-      <form>
-        <span>
-          <label>이름</label>
+    <div className="my-page">
+      <form className="my-page__form">
+        <span className="my-page__form--login-id">
+          <label>아이디 : </label>
+          <span>{userData && userData.login_id}</span>
         </span>
-        <span>
-          <label>아이디</label>
+        <span className="my-page__form--name">
+          <label>이름 : </label>
+          <span>{userData && userData.name}</span>
         </span>
-        <span>
-          <label>비밀번호</label>
+        <span className="my-page__form--phone_number">
+          <label>휴대폰 : </label>
+          {changeUserData ? (
+            <input
+              placeholder={userData && userData.phone_number}
+              onChange={onChange}
+              name="phone_number"
+              value={userData.phone_number}
+            />
+          ) : (
+            <span>{userData && userData.phone_number}</span>
+          )}
         </span>
-        <span>
-          <label>이메일</label>
+        <span className="my-page__form--email">
+          <label>이메일 : </label>
+          {changeUserData ? (
+            <input
+              placeholder={userData && userData.email}
+              onChange={onChange}
+              name="email"
+              value={userData.email}
+            />
+          ) : (
+            <span>{userData && userData.email}</span>
+          )}
         </span>
+        {changeUserData && (
+          <span>
+            <label>비밀번호 :</label>
+            <input
+              onChange={onChange}
+              name="password"
+              value={userData.password}
+            />
+          </span>
+        )}
         <span>
-          <label>휴대폰</label>
+          {changeUserData ? (
+            <button type="button" onClick={onClickSave}>
+              정보 저장하기
+            </button>
+          ) : (
+            <button type="button" onClick={onClickChange}>
+              정보 수정하기
+            </button>
+          )}
         </span>
       </form>
     </div>
