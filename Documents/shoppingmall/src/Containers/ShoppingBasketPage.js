b264eb21e8bc9ccec507984ff2bb931d657a9add
이지윤 <1yoouoo@gmail.com>
2022-12-31 18:50:44 +0900

    feat: checkbox 기능 구현

--- Documents/shoppingmall/src/Containers/ShoppingBasketPage.js
+++ Documents/shoppingmall/src/Containers/ShoppingBasketPage.js
@@ -1,12 +1,12 @@
+import { useState } from "react";
 import StyledSmallButton from "../Components/StyledSmallButton";
 
 const ShoppingBasketPage = () => {
   const dummyList = [
     {
       id: 1,
-      checked: false,
       image: "../Assets/shopping-list-sample-1.jpeg",
-      name: "이것은 테스트1 입니다.",
+      name: "이것은 테스트1 입니다.[color:red/size:M]",
       price: "30,000",
       count: "2",
       deliveryFee: "무료 배송",
@@ -14,7 +14,6 @@ const ShoppingBasketPage = () => {
     },
     {
       id: 2,
-      checked: false,
       image: "../Assets/shopping-list-sample-1.jpeg",
       name: "이것은 테스트2 입니다.",
       price: "40,000",
@@ -24,7 +23,6 @@ const ShoppingBasketPage = () => {
     },
     {
       id: 3,
-      checked: false,
       image: "../Assets/shopping-list-sample-1.jpeg",
       name: "이것은 테스트3 입니다.",
       price: "20,000",
@@ -33,6 +31,36 @@ const ShoppingBasketPage = () => {
       totalPrice: "20,000",
     },
   ];
+
+  //hook
+
+  //state
+  const [checkedItemList, setCheckedItemList] = useState([]);
+  //function
+  const handleCheckAll = (checked) => {
+    if (checked) {
+      // 전체 선택 클릭 시 데이터의 모든 아이템(id)를 담은 배열로 checkItems 상태 업데이트
+      const idArray = [];
+      dummyList.forEach((el) => idArray.push(el.id));
+      setCheckedItemList(idArray);
+    } else {
+      // 전체 선택 해제 시 checkItems 를 빈 배열로 상태 업데이트
+      setCheckedItemList([]);
+    }
+    console.log(checkedItemList);
+  };
+  const handleCheckItem = (checked, id) => {
+    if (checked) {
+      // 단일 선택 시 체크된 아이템을 배열에 추가
+      setCheckedItemList((prev) => [...prev, id]);
+      console.log("checked");
+    } else {
+      // 단일 선택 해제 시 체크된 아이템을 제외한 배열 (필터)
+      setCheckedItemList(checkedItemList.filter((el) => el !== id));
+      console.log("unchecked");
+    }
+    console.log(checkedItemList);
+  };
   return (
     <div className="shopping-basket">
       <div className="shopping-basket__form">
@@ -41,7 +69,16 @@ const ShoppingBasketPage = () => {
           <thead className="shopping-basket__thead">
             <tr>
               <th className="shopping-basket__thead--checkBox">
-                <input type="checkbox" />
+                <input
+                  type="checkbox"
+                  name="select-all"
+                  onChange={(e) => {
+                    handleCheckAll(e.target.checked);
+                  }}
+                  checked={
+                    checkedItemList.length === dummyList.length ? true : false
+                  }
+                />
               </th>
               <th className="shopping-basket__thead--img">이미지</th>
               <th className="shopping-basket__thead--name">상품정보</th>
@@ -57,7 +94,14 @@ const ShoppingBasketPage = () => {
               return (
                 <tr key={item.id}>
                   <td className="shopping-basket__tbody--checkBox">
-                    <input type="checkbox" />
+                    <input
+                      type="checkbox"
+                      name={`select-${item.id}`}
+                      onChange={(e) =>
+                        handleCheckItem(e.target.checked, item.id)
+                      }
+                      checked={checkedItemList.includes(item.id) ? true : false}
+                    />
                   </td>
                   <td className="shopping-basket__tbody--img">
                     <img
@@ -100,8 +144,8 @@ const ShoppingBasketPage = () => {
           </tbody>
           <tfoot className="shopping-basket__tfoot">
             <tr className="shopping-basket__tfoot--summary">
-              <td colspan="2">[기본 배송]</td>
-              <td colspan="6">
+              <td colSpan={2}>[기본 배송]</td>
+              <td colSpan={6}>
                 상품구매금액 <b>231,000</b>원 + 배송비 0원 = 합계 :
                 <span id="totalPrice"> 231,000원</span>
               </td>
@@ -120,8 +164,23 @@ const ShoppingBasketPage = () => {
             장바구니 비우기
           </StyledSmallButton>
         </div>
-        <div className="shopping-basket__form--total">
-          <form>총상품금액</form>
+        <div className="shopping-basket__total">
+          <table className="shopping-basket__total--table">
+            <thead>
+              <tr>
+                <td>총 상품금액</td>
+                <td>총 배송비</td>
+                <td>총 결제금액</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>231,000원</td>
+                <td>0원</td>
+                <td>= 231,000원</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
         <div className="shopping-basket__form--order">
           <span>
