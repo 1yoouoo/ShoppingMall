fa43e217822bc4437098bbbf85c9389488dfed11
이지윤 <1yoouoo@gmail.com>
2023-01-01 13:42:24 +0900

    feat: 장바구니 비우기 기능

--- Documents/shoppingmall/src/Containers/ShoppingBasketPage.js
+++ Documents/shoppingmall/src/Containers/ShoppingBasketPage.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import StyledButton from "../Components/StyledButton";
 import StyledSmallButton from "../Components/StyledSmallButton";
 
 const ShoppingBasketPage = () => {
-  const dummyList = [
+  const [dummyList, setDummyList] = useState([
     {
       id: 1,
       image: "../Assets/shopping-list-sample-1.jpeg",
@@ -30,9 +32,10 @@ const ShoppingBasketPage = () => {
       deliveryFee: "무료 배송",
       totalPrice: 20000,
     },
-  ];
+  ]);
 
   //state
+  const navigate = useNavigate();
   const [checkedItemList, setCheckedItemList] = useState([]);
   const [checkedIdList, setCheckedIdList] = useState([]);
   const [selectedItemsPrice, setSelectedItemsPrice] = useState(0);
@@ -42,6 +45,7 @@ const ShoppingBasketPage = () => {
     checkedItemList.map((item) => (eachItemPrice += item.totalPrice));
     setSelectedItemsPrice(eachItemPrice);
     console.log(selectedItemsPrice);
+    console.log(dummyList.length === 0);
   }, [checkedItemList]);
 
   //function
@@ -75,159 +79,189 @@ const ShoppingBasketPage = () => {
     }
     console.log("checkedIdList", checkedIdList);
   };
-  const addTotalPrice = () => {
-    let eachItemPrice = 0;
-    checkedItemList.map((item) => (eachItemPrice += item.totalPrice));
-    setSelectedItemsPrice(eachItemPrice);
-    console.log(selectedItemsPrice);
+  const hanbleDeletedAll = () => {
+    setDummyList([]);
   };
+  const hanbleDeletedItem = () => {};
   return (
-    <div className="shopping-basket">
-      <div className="shopping-basket__form">
-        <span className="shopping-basket__form--title">상품 ?개</span>
-        <table className="shopping-basket__form--table">
-          <thead className="shopping-basket__thead">
-            <tr>
-              <th className="shopping-basket__thead--checkBox">
-                <input
-                  type="checkbox"
-                  name="select-all"
-                  onChange={(e) => {
-                    handleCheckedAll(e.target.checked);
-                  }}
-                  checked={
-                    checkedIdList.length === dummyList.length ? true : false
-                  }
-                />
-              </th>
-              <th className="shopping-basket__thead--img">이미지</th>
-              <th className="shopping-basket__thead--name">상품정보</th>
-              <th className="shopping-basket__thead--price">판매가</th>
-              <th className="shopping-basket__thead--count">수량</th>
-              <th className="shopping-basket__thead--deliveryFee">배송비</th>
-              <th className="shopping-basket__thead--totalPrice">합계</th>
-              <th className="shopping-basket__thead--button">선택</th>
-            </tr>
-          </thead>
-          <tbody className="shopping-basket__tbody">
-            {dummyList.map((item) => {
-              return (
-                <tr key={item.id}>
-                  <td className="shopping-basket__tbody--checkBox">
+    <>
+      {dummyList.length !== 0 ? (
+        <div className="shopping-basket">
+          <div className="shopping-basket__form">
+            <span className="shopping-basket__form--title">
+              상품 {dummyList.length}개
+            </span>
+            <table className="shopping-basket__form--table">
+              <thead className="shopping-basket__thead">
+                <tr>
+                  <th className="shopping-basket__thead--checkBox">
                     <input
                       type="checkbox"
-                      name={`select-${item.id}`}
-                      onChange={(e) =>
-                        handleCheckedItem(e.target.checked, item.id)
+                      name="select-all"
+                      onChange={(e) => {
+                        handleCheckedAll(e.target.checked);
+                      }}
+                      checked={
+                        checkedIdList.length === dummyList.length ? true : false
                       }
-                      checked={checkedIdList.includes(item.id) ? true : false}
                     />
-                  </td>
-                  <td className="shopping-basket__tbody--img">
-                    <img
-                      src={require("../Assets/shopping-list-sample-1.jpeg")}
-                      alt=""
-                    />
-                  </td>
-                  <td className="shopping-basket__tbody--name">{item.name}</td>
-                  <td className="shopping-basket__tbody--price">
-                    {item.price}원
-                  </td>
-                  <td className="shopping-basket__tbody--count">
-                    {item.count}
-                  </td>
-                  <td className="shopping-basket__tbody--deliveryFee">
-                    {item.deliveryFee}
-                  </td>
-                  <td className="shopping-basket__tbody--totalPrice">
-                    {item.totalPrice}원
-                  </td>
-                  <td className="shopping-basket__tbody--button">
-                    <StyledSmallButton
-                      background="#e6e6e6"
-                      color="black"
-                      border="none"
-                    >
-                      주문하기
-                    </StyledSmallButton>
-                    <StyledSmallButton
-                      background="#e6e6e6"
-                      color="black"
-                      border="none"
-                    >
-                      삭제하기
-                    </StyledSmallButton>
+                  </th>
+                  <th className="shopping-basket__thead--img">이미지</th>
+                  <th className="shopping-basket__thead--name">상품정보</th>
+                  <th className="shopping-basket__thead--price">판매가</th>
+                  <th className="shopping-basket__thead--count">수량</th>
+                  <th className="shopping-basket__thead--deliveryFee">
+                    배송비
+                  </th>
+                  <th className="shopping-basket__thead--totalPrice">합계</th>
+                  <th className="shopping-basket__thead--button">선택</th>
+                </tr>
+              </thead>
+              <tbody className="shopping-basket__tbody">
+                {dummyList.map((item) => {
+                  return (
+                    <tr key={item.id}>
+                      <td className="shopping-basket__tbody--checkBox">
+                        <input
+                          type="checkbox"
+                          name={`select-${item.id}`}
+                          onChange={(e) =>
+                            handleCheckedItem(e.target.checked, item.id)
+                          }
+                          checked={
+                            checkedIdList.includes(item.id) ? true : false
+                          }
+                        />
+                      </td>
+                      <td className="shopping-basket__tbody--img">
+                        <img
+                          src={require("../Assets/shopping-list-sample-1.jpeg")}
+                          alt=""
+                        />
+                      </td>
+                      <td className="shopping-basket__tbody--name">
+                        {item.name}
+                      </td>
+                      <td className="shopping-basket__tbody--price">
+                        {item.price}원
+                      </td>
+                      <td className="shopping-basket__tbody--count">
+                        {item.count}
+                      </td>
+                      <td className="shopping-basket__tbody--deliveryFee">
+                        {item.deliveryFee}
+                      </td>
+                      <td className="shopping-basket__tbody--totalPrice">
+                        {item.totalPrice}원
+                      </td>
+                      <td className="shopping-basket__tbody--button">
+                        <StyledSmallButton
+                          background="#e6e6e6"
+                          color="black"
+                          border="none"
+                        >
+                          주문하기
+                        </StyledSmallButton>
+                        <StyledSmallButton
+                          background="#e6e6e6"
+                          color="black"
+                          border="none"
+                        >
+                          삭제하기
+                        </StyledSmallButton>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+              <tfoot className="shopping-basket__tfoot">
+                <tr className="shopping-basket__tfoot--summary">
+                  <td colSpan={2}>[기본 배송]</td>
+                  <td colSpan={6}>
+                    상품구매금액 <b>{selectedItemsPrice}</b>원 + 배송비 0원 =
+                    합계 :<span id="totalPrice"> {selectedItemsPrice}원</span>
                   </td>
                 </tr>
-              );
-            })}
-          </tbody>
-          <tfoot className="shopping-basket__tfoot">
-            <tr className="shopping-basket__tfoot--summary">
-              <td colSpan={2}>[기본 배송]</td>
-              <td colSpan={6}>
-                상품구매금액 <b>231,000</b>원 + 배송비 0원 = 합계 :
-                <span id="totalPrice"> {selectedItemsPrice}원</span>
-              </td>
-            </tr>
-          </tfoot>
-        </table>
-        <div className="shopping-basket__form--delete">
-          <StyledSmallButton
-            background="#e6e6e6"
-            color="black"
-            border="none"
-            onClick={addTotalPrice}
-          >
-            삭제하기
-          </StyledSmallButton>
-          <StyledSmallButton
+              </tfoot>
+            </table>
+            <div className="shopping-basket__form--delete">
+              <StyledSmallButton
+                background="#e6e6e6"
+                color="black"
+                border="none"
+              >
+                삭제하기
+              </StyledSmallButton>
+              <StyledSmallButton
+                background="white"
+                color="black"
+                border="1px solid #ccc"
+                onClick={hanbleDeletedAll}
+              >
+                장바구니 비우기
+              </StyledSmallButton>
+            </div>
+            <div className="shopping-basket__total">
+              <table className="shopping-basket__total--table">
+                <thead>
+                  <tr>
+                    <td>총 상품금액</td>
+                    <td>총 배송비</td>
+                    <td>총 결제금액</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{selectedItemsPrice}원</td>
+                    <td>0원</td>
+                    <td>= {selectedItemsPrice}원</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="shopping-basket__form--order">
+              <span>
+                <StyledSmallButton
+                  background="black"
+                  color="white"
+                  border="none"
+                >
+                  전체상품주문
+                </StyledSmallButton>
+                <StyledSmallButton
+                  background="#e6e6e6"
+                  color="black"
+                  border="none"
+                >
+                  선택상품주문
+                </StyledSmallButton>
+              </span>
+              <span>
+                <StyledSmallButton
+                  background="white"
+                  color="black"
+                  border="1px solid #ccc"
+                >
+                  쇼핑계속하기
+                </StyledSmallButton>
+              </span>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="empty-basket">
+          <img src={require("../Assets/empty-basket-baemin.png")} alt="" />
+          <StyledButton
             background="white"
             color="black"
             border="1px solid #ccc"
+            onClick={() => navigate("/homepage")}
           >
-            장바구니 비우기
-          </StyledSmallButton>
+            홈으로 가기
+          </StyledButton>
         </div>
-        <div className="shopping-basket__total">
-          <table className="shopping-basket__total--table">
-            <thead>
-              <tr>
-                <td>총 상품금액</td>
-                <td>총 배송비</td>
-                <td>총 결제금액</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>231,000원</td>
-                <td>0원</td>
-                <td>= 231,000원</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="shopping-basket__form--order">
-          <span>
-            <StyledSmallButton background="black" color="white" border="none">
-              전체상품주문
-            </StyledSmallButton>
-            <StyledSmallButton background="#e6e6e6" color="black" border="none">
-              선택상품주문
-            </StyledSmallButton>
-          </span>
-          <span>
-            <StyledSmallButton
-              background="white"
-              color="black"
-              border="1px solid #ccc"
-            >
-              쇼핑계속하기
-            </StyledSmallButton>
-          </span>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
