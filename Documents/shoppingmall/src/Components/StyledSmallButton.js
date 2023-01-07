ae320315d66cd63f6229d4d045c5566012737200
Blanc <1yoouoo@gmail.com>
2023-01-07 15:38:27 +0900

    Delete Documents/shoppingmall directory

--- Documents/shoppingmall/src/Components/StyledSmallButton.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import StyledButton from "./StyledButton";
-
-const StyledSmallButton = styled(StyledButton)`
-  width: 160px;
-`;
-
-export default StyledSmallButton;
